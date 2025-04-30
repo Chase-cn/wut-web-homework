88e1f4fd48f68f3571306cd3d61816a2d8cdc00c
Chase_55 <14172331+chase55@user.noreply.gitee.com>
2025-04-30 11:15:26 +0800

    test: 使用jemeter进行了基础的api测试

--- src/main/java/com/whut/experiment2/exception/BusinessException.java
+++ src/main/java/com/whut/experiment2/exception/BusinessException.java
@@ -8,8 +8,17 @@ import lombok.EqualsAndHashCode;
 @Data                          // 自动生成 getter/setter
 @EqualsAndHashCode(callSuper = false)  // 避免父类(RuntimeException)的equals/hashCode影响
 public abstract class BusinessException extends RuntimeException{
+    /**
+     * 这里其实更应该是错误码,如"USERNAME_EXISTS",而不是状态码
+     * 状态码在GlobalHander中@ResponseStatus()注解实现
+     */
     private final int errorCode;
 
+    /**
+     *
+     * @param errorCode 错误码(更好),而不状态码(多余)
+     * @param message 错误信息
+     */
     public BusinessException(int errorCode, String message) {
         super(message);
         this.errorCode = errorCode;
